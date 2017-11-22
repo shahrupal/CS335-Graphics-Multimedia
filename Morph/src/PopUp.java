28639e51de0da56a8f545742d676a6af241ee7ed
Rupal Shah <rupal.shah@uky.edu>
2017-11-22 14:34:13 -0500

    animates control points from left panel to right panel correctly

--- Morph/src/PopUp.java
+++ Morph/src/PopUp.java
@@ -19,6 +19,8 @@ public class PopUp extends JPanel {
     int OFFSET = DIAMETER / 2;
     private ArrayList<Point> morphPointsStart = new ArrayList<Point>();
     private ArrayList<Point> morphPointsEnd = new ArrayList<Point>();
+    private ArrayList<Integer> morphPointsX = new ArrayList<Integer>();
+    private ArrayList<Integer> morphPointsY = new ArrayList<Integer>();
 
     public PopUp() {
 
@@ -47,9 +49,12 @@ public class PopUp extends JPanel {
 
                     morphPointsStart.add(start[i][j]);
                     morphPointsEnd.add(end[i][j]);
+                    morphPointsX.add(i);
+                    morphPointsY.add(j);
 
-                    tempI = i;
-                    tempJ = j;
+
+//                    tempI = i;
+//                    tempJ = j;
                     ActionListener timer = new ActionListener() {
                         public void actionPerformed(ActionEvent e) {
                             if (time != 1) {
@@ -107,51 +112,25 @@ public class PopUp extends JPanel {
                 double x = morphPointsStart.get(l).getX() + (time * (morphPointsEnd.get(l).getX() - morphPointsStart.get(l).getX()));
                 double y = morphPointsStart.get(l).getY() + (time * (morphPointsEnd.get(l).getY() - morphPointsStart.get(l).getY()));
 
-                start[tempI][tempJ].setLocation(x, y);
+                start[morphPointsX.get(l)][morphPointsY.get(l)].setLocation(x, y);
 
-                g2d.drawOval(start[tempI][tempJ].x - OFFSET, start[tempI][tempJ].y - OFFSET, DIAMETER, DIAMETER);
-                g2d.fillOval(start[tempI][tempJ].x - OFFSET, start[tempI][tempJ].y - OFFSET, DIAMETER, DIAMETER);
+            }
 
-//                if(tempJ - 1 > -1){ g2d.drawLine(start[tempI][tempJ].x, start[tempI][tempJ].y, start[tempI][tempJ - 1].x, start[tempI][tempJ - 1].y); }
-//                if(tempI + 1 < size){ g2d.drawLine(start[tempI][tempJ].x, start[tempI][tempJ].y, start[tempI + 1][tempJ].x, start[tempI + 1][tempJ].y); }
-//                if((tempJ + 1) < size){ g2d.drawLine(start[tempI][tempJ].x, start[tempI][tempJ].y, start[tempI][tempJ + 1].x, start[tempI][tempJ + 1].y); }
-//                if((tempI - 1) > -1){ g2d.drawLine(start[tempI][tempJ].x, start[tempI][tempJ].y, start[tempI - 1][tempJ].x, start[tempI - 1][tempJ].y); }
+            for (int a = 0; a < size; a++) {
+                for (int b = 0; b < size; b++) {
+                    g2d.drawOval(start[a][b].x - OFFSET, start[a][b].y - OFFSET, DIAMETER, DIAMETER);
+                    g2d.fillOval(start[a][b].x - OFFSET, start[a][b].y - OFFSET, DIAMETER, DIAMETER);
+                    if(b - 1 > -1){ g2d.drawLine(start[a][b].x, start[a][b].y, start[a][b - 1].x, start[a][b - 1].y); }
+                    if(a + 1 < size){ g2d.drawLine(start[a][b].x, start[a][b].y, start[a + 1][b].x, start[a + 1][b].y); }
+                    if((b + 1) < size){ g2d.drawLine(start[a][b].x, start[a][b].y, start[a][b + 1].x, start[a][b + 1].y); }
+                    if((a - 1) > -1){ g2d.drawLine(start[a][b].x, start[a][b].y, start[a - 1][b].x, start[a - 1][b].y); }
 
-                for (int c = 0; c < size; c++) {
-                for (int d = 0; d < size; d++) {
-                    if(d - 1 > -1){ g2d.drawLine(start[c][d].x, start[c][d].y, start[c][d - 1].x, start[c][d - 1].y); }
-                    if(c + 1 < size){ g2d.drawLine(start[c][d].x, start[c][d].y, start[c + 1][d].x, start[c + 1][d].y); }
-                    if((d + 1) < size){ g2d.drawLine(start[c][d].x, start[c][d].y, start[c][d + 1].x, start[c][d + 1].y); }
-                    if((c - 1) > -1){ g2d.drawLine(start[c][d].x, start[c][d].y, start[c - 1][d].x, start[c - 1][d].y); }
                 }
             }
 
-
-            }
         }
-
-
-//            time += 1 / frames;
-//
-//            double x = start[tempI][tempJ].getX() + (time * (end[tempI][tempJ].getX() - start[tempI][tempJ].getX()));
-//            double y = start[tempI][tempJ].getY() + (time * (end[tempI][tempJ].getY() - start[tempI][tempJ].getY()));
-//
-//            start[tempI][tempJ].setLocation(x, y);
-//
-//            for (int c = 0; c < size; c++) {
-//                for (int d = 0; d < size; d++) {
-//                    g2d.drawOval(start[c][d].x - OFFSET, start[c][d].y - OFFSET, DIAMETER, DIAMETER);
-//                    g2d.fillOval(start[c][d].x - OFFSET, start[c][d].y - OFFSET, DIAMETER, DIAMETER);
-//                    if(d - 1 > -1){ g2d.drawLine(start[c][d].x, start[c][d].y, start[c][d - 1].x, start[c][d - 1].y); }
-//                    if(c + 1 < size){ g2d.drawLine(start[c][d].x, start[c][d].y, start[c + 1][d].x, start[c + 1][d].y); }
-//                    if((d + 1) < size){ g2d.drawLine(start[c][d].x, start[c][d].y, start[c][d + 1].x, start[c][d + 1].y); }
-//                    if((c - 1) > -1){ g2d.drawLine(start[c][d].x, start[c][d].y, start[c - 1][d].x, start[c - 1][d].y); }
-//                }
-//            }
-//
-//        }
-
     }
+
 
 
 }
